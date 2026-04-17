fb5ea38a90445befe4e52bc83c310cd5aacc5525
ploymahloy <patrick.mahloy@gmail.com>
2026-04-16 23:26:45 -0400

    path typo

--- tests/environment.test.ts
+++ tests/environment.test.ts
@@ -2,13 +2,12 @@ import { describe, it, expect } from 'vitest';
 
 describe('Environment Setup', () => {
 	it('should support ES Modules', () => {
-		const message = "FSS is ready!";
+		const message = 'FSS is ready!';
 		expect(message).toBeTypeOf('string');
 	});
 
 	it('can import local files', async () => {
-		// fails while src/index.ts is empty
-		const module = await import('..src/index.js'); // .js required for NodeNext
+		const module = await import('../src/index.js');
 		expect(module).toBeDefined();
 	});
 });
